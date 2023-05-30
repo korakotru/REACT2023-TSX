4eff4dfdbe0ac22e983ffeb6f510e8523228821e
korakot-kkps <korakot.lonl@kkpfg.com>
2023-05-30 14:35:40 +0700

    commit
    Add primereact

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ITodoItem from "../../types/TodoItem";
 import TodoListService from "../../services/TodoListService";
-    
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "/node_modules/primeflex/primeflex.css";
+import { Card } from "primereact/card";
+import { Button } from "primereact/button";
+
+import { Ripple } from "primereact/ripple";
+
 const TodoList: React.FC = () => {
   const [todoList, setTodoList] = useState<Array<ITodoItem>>([]);
 
@@ -24,15 +33,21 @@ const TodoList: React.FC = () => {
     <>
       <h4>Todo List</h4>
 
-      <ul className="list-group">
+      <div className="grid p-justify-center">
         {todoList &&
           todoList.map((todoItem, index) => (
-            <li key={todoItem.id}>
-              {todoItem.name} -{" "}
-              {todoItem.isComplete ? "Completed" : "In-Complete"}
-            </li>
+            <div className="col-3" key={todoItem.id}>
+              <div className="card flex justify-content-center align-items-center">
+                <div className="bg-primary flex select-none justify-content-center align-items-center shadow-2 border-round p-2 font-bold p-ripple w-full">
+                  <div className=" pi pi-spin pi-cog" />
+                  &nbsp;
+                  {todoItem.name}
+                  <Ripple />
+                </div>
+              </div>
+            </div>
           ))}
-      </ul>
+      </div>
     </>
   );
 };
